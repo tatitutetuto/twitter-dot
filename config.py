6ef88de04cff72ae73e393d1e1a4a537a797d208
sasisusesuso <95947346+sasisusesuso@users.noreply.github.com>
2022-01-05 22:25:02 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Twitterいいね数など
 FAVORITE_COUNT = 40
 GET_TWEET_COUNT = 80
-SEARCH_WORD = '#DOT 　-#BTC　-#XRP　-filter:retweets'
+SEARCH_WORD = '#DOT -エアドロ　-#BTC　-#XRP　-filter:retweets'
 
 # スクレイピング用のURL
 BITTIMES_URL = 'https://bittimes.net/tag/polkadot'
