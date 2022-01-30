9219f8a7ac60a041bf65a24e9155467c6f1ea853
sasisusesuso <95947346+sasisusesuso@users.noreply.github.com>
2022-01-30 21:39:02 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ FAVORITE_COUNT = 40
 GET_TWEET_COUNT = 80
 SEARCH_WORD = '#DOT -エアドロ　-#BTC　-#XRP　-filter:retweets'
 
-# スクレイピング用のURL
+# スクレイピング用のBITTIMES_URL
 BITTIMES_URL = 'https://bittimes.net/tag/polkadot'
+
+# スクレイピング用のYAHOO_URL
+YAHOO_URL = 'https://news.yahoo.co.jp/search?p=ポルカドット+仮想通貨+-%22%28DOT）、%22+-%22（DOT：%22&ei=utf-8'
