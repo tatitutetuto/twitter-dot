da1f14c67fa5c177d1340fa656a4db1db8c0df45
sasisusesuso <95947346+sasisusesuso@users.noreply.github.com>
2022-03-27 22:47:40 +0900

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ BITTIMES_URL = 'https://bittimes.net/tag/polkadot'
 
 # スクレイピング用のYAHOO_URL
 YAHOO_URL = 'https://news.yahoo.co.jp/search?p=ポルカドット+仮想通貨+-%22%28DOT）、%22+-%22（DOT：%22&ei=utf-8'
+
+# リツイートする対象検索キーワード
+SEARCH_WORD = 'from:neweconomy_g OR from:coin_post OR from:CoinDeskjapan'
